2ab2f4d43a81eaa7af0523e9b9ba8d733cb03bd8
bnmwag <bnm.wag@gmail.com>
2024-02-08 00:15:16 +0100

    revert nav

--- apps/web/src/lib/components/layout/navbar.svelte
+++ apps/web/src/lib/components/layout/navbar.svelte
@@ -1,15 +1,5 @@
 <script lang="ts">
-  export let pathname: string;
+  // export let pathname: string;
 </script>
 
-<header class="fixed top-0 left-0 w-screen py-12 text-white z-[100] px-[5vw]">
-  <div class="flex items-center justify-between">
-    <div>KF-Media</div>
-    <nav>
-      <ul class="flex items-center">
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </nav>
-  </div>
-</header>
+<header class="fixed top-0 left-0 w-screen py-12 text-white z-[100] px-[5vw]"></header>
