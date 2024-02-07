bf0b96a4712e298f129b9b40fbbdf63846bca8a6
bnmwag <bnm.wag@gmail.com>
2024-02-08 00:29:15 +0100

    single logo verve with bg grid

--- apps/web/src/lib/components/blocks/welcome.svelte
+++ apps/web/src/lib/components/blocks/welcome.svelte
@@ -6,40 +6,66 @@
   const { message } = $$props as Props;
 </script>
 
-<div class="h-screen bg-[#0b0b0a] grid place-items-center">
-  <div class="flex items-center gap-x-[2vw]">
+<div class="h-screen bg-[#0b0b0a] grid place-items-center text-white">
+  <div
+    class="absolute inset-0 [background:radial-gradient(circle,rgba(11,11,10,0)0%,rgba(11,11,10,1)40%,rgba(11,11,10,1)100%)] z-10"
+  />
+  <svg
+    width="100%"
+    height="100%"
+    xmlns="http://www.w3.org/2000/svg"
+    class="absolute inset-0 opacity-[0.085]"
+  >
+    <defs>
+      <pattern
+        id="smallGrid"
+        width="10"
+        height="10"
+        patternUnits="userSpaceOnUse"
+      >
+        <path
+          d="M 10 0 L 0 0 0 10"
+          fill="none"
+          stroke="lightblue"
+          stroke-width="0.5"
+        />
+      </pattern>
+      <pattern id="grid" width="100" height="100" patternUnits="userSpaceOnUse">
+        <rect width="100" height="100" fill="url(#smallGrid)" />
+        <path
+          d="M 100 0 L 0 0 0 100"
+          fill="none"
+          stroke="lightblue"
+          stroke-width="1"
+        />
+      </pattern>
+    </defs>
+
+    <rect width="100%" height="100%" fill="url(#grid)" />
+  </svg>
+  <div class="flex items-center flex-col gap-y-12">
     <div class="relative w-fit">
       <div
-        class=" w-[5vw] h-[5vw] rounded-[1vw] bg-gradient-to-br from-[#141414] to-[#2b2b2b] z-10 relative p-[2px]"
+        class=" w-48 h-48 rounded-3xl bg-gradient-to-b from-[#141414] to-[#202020] z-10 relative p-[2px]"
       >
         <div
-          class="h-full w-full bg-[#0b0b0a] rounded-[calc(1vw-1px)] grid place-items-center"
+          class="h-full w-full bg-[#0b0b0a] rounded-[23px] grid place-items-center"
         >
           <img
-            src="/logo_prm_compact_dark.svg"
-            alt="zap.studio Logo"
-            class="w-[33%] h-[33%]"
+            src="/verve_logo.svg"
+            alt="verve Logo"
+            class="w-[45%] h-[45%] animate-pulse"
+            style="animation-duration: 3000ms;"
           />
         </div>
       </div>
       <div
-        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[calc(50%-2vw)] bg-purple-700 w-24 h-12 rounded-full blur-[50px]"
-      />
-    </div>
-    <span class="text-[2vw] font-thin text-neutral-900">+</span>
-    <div class="relative w-fit">
-      <div
-        class=" w-[5vw] h-[5vw] rounded-[1vw] bg-gradient-to-b from-[#141414] to-[#202020] z-10 relative p-[2px]"
-      >
-        <div
-          class="h-full w-full bg-[#0b0b0a] rounded-[calc(1vw-1px)] grid place-items-center"
-        >
-          <img src="/verve_logo.svg" alt="verve Logo" class="w-[45%] h-[45%]" />
-        </div>
-      </div>
-      <div
-        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[calc(50%-1vw)] bg-rose-500 w-24 h-12 rounded-full blur-[50px]"
+        class="animate-pulse absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[calc(50%-40px)] bg-rose-500 w-24 h-12 rounded-full blur-[50px]"
+        style="animation-duration: 3000ms; animation-delay: 750ms;"
       />
     </div>
   </div>
 </div>
+
+<style>
+</style>
